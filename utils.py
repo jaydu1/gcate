19e6a9fb20c3a37ae073e928fd217bd3e8a1ecf7
jaydu1 <413075930@qq.com>
2023-04-17 15:05:18 -0400

    Add early stopping.

---
+++ utils.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+class Early_Stopping():
+    '''
+    The early-stopping monitor.
+    '''
+    def __init__(self, warmup=25, patience=25, tolerance=0., is_minimize=True):
+        self.warmup = warmup
+        self.patience = patience
+        self.tolerance = tolerance
+        self.is_minimize = is_minimize
+
+        self.step = -1
+        self.best_step = -1
+        self.best_metric = np.inf
+
+        if not self.is_minimize:
+            self.factor = -1.0
+        else:
+            self.factor = 1.0
+
+    def __call__(self, metric):
+        self.step += 1
+        
+        if self.step < self.warmup:
+            return False
+        elif self.factor*metric<self.factor*self.best_metric-self.tolerance:
+            self.best_metric = metric
+            self.best_step = self.step
+            return False
+        elif self.step - self.best_step>self.patience:
+            print('Best Epoch: %d. Best Metric: %f.'%(self.best_step, self.best_metric))
+            return True
+        else:
+            return False
